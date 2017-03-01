a93ee52ce65eacb2635b571f87a4e2f04b7cd5a3
Roger Ng <roger.ng@helloworld.com.au>
2017-03-01 15:15:24 +1100

    banners json added

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,4 +12,32 @@ Api.get('/get-all', (req, res) => {
   });
 });
 
+Api.get('/get-hw-rotate-banners', (req, res) => {
+  res.json({
+    status: 200,
+    data: {
+      result: [
+        {
+          desktop: 'http://lorempixel.com/1000/600/nature/1/',
+          mobile: 'http://lorempixel.com/500/100/nature/5/',
+          ctaImg: 'http://lorempixel.com/304/510/nature/1/',
+          cta: 'http://google.com'
+        },
+        {
+          desktop: 'http://lorempixel.com/1000/600/nature/2/',
+          mobile: 'http://lorempixel.com/500/100/nature/6/',
+          ctaImg: 'http://lorempixel.com/304/510/nature/2/',
+          cta: 'http://yahoo.com'
+        },
+        {
+          desktop: 'http://lorempixel.com/1000/600/nature/3/',
+          mobile: 'http://lorempixel.com/500/100/nature/7/',
+          ctaImg: 'http://lorempixel.com/304/510/nature/3/',
+          cta: 'http://microsoft.com'
+        }
+      ]
+    }
+  });
+});
+
 export default Api;
