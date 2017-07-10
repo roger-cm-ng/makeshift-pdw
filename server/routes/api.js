276e712acaf3ce7015b1b383d052460270544c33
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2017-07-10 11:38:33 +1000

    delete functionality

--- server/routes/api.js
+++ server/routes/api.js
@@ -36,7 +36,6 @@ Api.get('/pages', (req, res) => {
 });
 
 Api.get('/pages/:id', (req, res) => {
-  console.log('id: ', req.params.id);
 	Pages.findOne({ _id: req.params.id })
 		.then((obj) => {
 			res.json({
@@ -60,36 +59,56 @@ Api.post('/pages', (req, res) => {
 		modules: req.body.modules
   }
 
-	let data;
+	const data = new Pages(item);
 
-	if (req.body._id) {
-		item._id = req.body._id
+  data.save((err, pages) => {
+    Pages.find()
+      .then((arr) => {
+				let content = {};
+				const page = arr.find((i) => i.created === req.body.created);
+        res.json({
+          status: 200,
+          results: page
+        });
+      })
+			.then()
+  });
+});
 
-		data = new Pages(item);
+Api.put('/pages', (req, res) => {
+	const item = {
+		_id: req.body._id,
+		studentPageName: req.body.studentPageName,
+		teacherPageName: req.body.teacherPageName,
+		author: req.body.author,
+		type: req.body.type,
+		grade: req.body.grade,
+		topic: req.body.topic,
+		subTopic: req.body.subTopic,
+		created: req.body.created,
+		status: req.body.status,
+		modules: req.body.modules
+  }
 
-		Pages.findByIdAndUpdate(req.body._id, data)
-			.then((arr) => {
-				res.json({
-					status: 200,
-					results: arr
-				});
-			})
-	} else {
-		data = new Pages(item);
+	const data = new Pages(item);
 
-	  data.save((err, pages) => {
-	    Pages.find()
-	      .then((arr) => {
-					let content = {};
-					const page = arr.find((i) => i.created === req.body.created);
-	        res.json({
-	          status: 200,
-	          results: page
-	        });
-	      })
-				.then()
-	  });
-	}
+	Pages.findByIdAndUpdate(req.body._id, data)
+		.then((arr) => {
+			res.json({
+				status: 200,
+				results: arr
+			});
+		})
+});
+
+Api.delete('/pages/:id', (req, res) => {
+	Pages.findByIdAndRemove({ _id: req.params.id })
+		.then((obj) => {
+			res.json({
+				status: 200,
+				results: obj
+			});
+		})
 });
 
 export default Api;
