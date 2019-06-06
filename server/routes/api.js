e375055a6f08361856426827ff7a64f89f28ed36
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2019-06-06 17:40:03 +1000

    activity data

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,11 +7,16 @@ const Api = express.Router();
 Api.use(cors());
 
 Api.post('/request-data', (req, res) => {
-  let data = {};
+  let data = [];
   if (req.body.girlId === 'GIRL123') {
     data = activity;
   }
   res.json(data);
+});
+
+Api.post('/activity-data', (req, res) => {
+  console.log(req.body);
+  res.json({ foo: 'bar' });
 });
 
 export default Api;
