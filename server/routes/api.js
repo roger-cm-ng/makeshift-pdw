229f2c2b2854e76df324e48e9c6ba0458114d9a3
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2019-06-24 13:41:12 +1000

    error code

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,17 +6,17 @@ const Api = express.Router();
 
 Api.use(cors());
 
-Api.post('/request-data', (req, res) => {
+Api.get('/modules/:module/resources/:girlId', (req, res) => {
   let data = [];
-  if (req.body.girlId === 'GIRL123') {
+  console.log(req.params.girlId);
+  if (req.params.girlId === 'GIRL123') {
     data = activity;
   }
   res.json(data);
 });
 
-Api.post('/activity-data', (req, res) => {
-  console.log(req.body);
-  res.json({ foo: 'bar' });
+Api.get('/error/:code', (req, res) => {
+  res.status(parseInt(req.params.code)).json({ foo: 'bar' });
 });
 
 export default Api;
