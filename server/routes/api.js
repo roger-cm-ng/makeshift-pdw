ea3a4a8f2df91a321c2fdc15dca86a250556870f
Roger Ng <roger.ng@3plearning.com>
2019-11-13 10:55:17 +1100

    Without checking for request payload

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,35 +9,36 @@ const Api = express.Router();
 Api.use(cors());
 
 Api.post('/assignmentDetails', (req, res) => {
-  if (req.body.assignmentId === 'abc123') {
-    res.status(200).json(assignmentDetails);
-  } else {
-    res.status(404).json({
-      error: 'no assignment found'
-    });
-  }  
+  // console.dir(req);
+  // if (req.body.assignmentId === 'abc123') {
+  res.status(200).json(assignmentDetails);
+  // } else {
+  //   res.status(404).json({
+  //     error: 'no assignment found'
+  //   });
+  // }  
 });
 
 Api.post('/getQuestion', (req,res) => {
-  if (req.body.assignmentId === 'abc123' && req.body.locale === 'en-AU') {
-    res.status(200).json(activityQuestions.nonDistractor[0]);
-  } else {
-    res.status(404).json({
-      error: 'no assignment found'
-    });
-  } 
+  // if (req.body.assignmentId === 'abc123' && req.body.locale === 'en-AU') {
+  res.status(200).json(activityQuestions.nonDistractor[0]);
+  // } else {
+  //   res.status(404).json({
+  //     error: 'no assignment found'
+  //   });
+  // } 
 });
 
 Api.post('/saveQuestion', (req,res) => {
-  if (req.body.assignmentId === 'abc123' && req.body.locale === 'en-AU') {
+  // if (req.body.assignmentId === 'abc123' && req.body.locale === 'en-AU') {
     res.status(200).json(activityQuestionsSorter({
       isCorrect: req.body.isCorrect
     }));
-  } else {
-    res.status(404).json({
-      error: 'no assignment found'
-    });
-  } 
+  // } else {
+  //   res.status(404).json({
+  //     error: 'no assignment found'
+  //   });
+  // } 
 });
 
 export default Api;
