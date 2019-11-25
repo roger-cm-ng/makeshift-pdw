c0998f2b4ca51ae98f6498b9e9724fea15b21389
Roger Ng <roger.ng@3plearning.com>
2019-11-25 15:52:23 +1100

    streamline

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,32 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import assignmentDetailsCrown1 from './assignment-details-crown1.json';
-import assignmentDetailsCrown2 from './assignment-details-crown2.json';
-import assignmentDetailsCrown3 from './assignment-details-crown3.json';
-import assignmentDetailsCrown4 from './assignment-details-crown4.json';
-import assignmentDetailsCrown5 from './assignment-details-crown5.json';
-import { activityQuestionsSorter } from './activity-questions-sorter';
+import _ from 'lodash';
+import assignmentDetailsCrown from './assignment-details-crown.json';
 
 const Api = express.Router();
 
 Api.use(cors());
 
-const assignmentDetailsCrowns = {
-  assignmentDetailsCrown1, 
-  assignmentDetailsCrown2, 
-  assignmentDetailsCrown3, 
-  assignmentDetailsCrown4, 
-  assignmentDetailsCrown5
-}
-
 Api.post('/assignmentDetails', (req, res) => {
   const crownNum = /Crown[1-5]/.exec(req.body.assignmentId);
+  const gemNum = /Gem[1-5]/.exec(req.body.assignmentId);
+  const details = _.clone(assignmentDetailsCrown);
 
-  if (crownNum) {
-    res.status(200).json(assignmentDetailsCrowns[`assignmentDetails${crownNum[0]}`]);
-  } else {
-    res.status(200).json(assignmentDetailsCrown5);
-  }  
+  if (crownNum) {    
+    details.stepCount = Number(crownNum[0][5]);
+  } 
+  if (gemNum) {
+    details.stepsCompleted = Number(gemNum[0][3]);
+  }
+  res.status(200).json(details);
 });
 
 Api.post('/getQuestion', (req,res) => {
