8d4b9b40c04286f0e74ce68bdc47071bd48e2011
Roger Ng <roger.ng@3plearning.com>
2020-04-27 21:37:08 +1000

    Changed route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
+import queryString from 'querystring';
 
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
@@ -13,6 +14,13 @@ const Api = express.Router();
 Api.use(cors());
 
 const members = ['er', 'sm', 'gt', 'ra', 'ie', 'rn', 'va', 'sb'];
+
+const getValueFromReferer = (refererUrl, key) => {
+  const queryParams = refererUrl;
+  const query = queryParams.replace('?', '&');
+  const params = queryString.parse(query);
+  return params[key];
+};
 
 Api.get('/members', (req, res) => {
   res.status(200).json(members);
@@ -39,6 +47,36 @@ Api.post('/set-default', (req, res) => {
   memberKey.set(endPointKey, defaultValue).write();
 
   res.status(200).json(memberKey.get(endPointKey).value());
+});
+
+Api.get('/assessments', (req, res) => {
+  const member = getValueFromReferer(req.headers.referer, 'nobushi');
+
+  res.status(200).json(db.get('members').get(member).get('allAssessments').value());
+});
+
+Api.get('/classrooms', (req, res) => {
+  const member = getValueFromReferer(req.headers.referer, 'nobushi');
+
+  res.status(200).json(db.get('members').get(member).get('classrooms').value());
+});
+
+Api.get('/assessments/:id/versions/:version', (req, res) => {
+  const member = getValueFromReferer(req.headers.referer, 'nobushi');
+
+  res.status(200).json(db.get('members').get(member).get('oneAssessment').value());
+});
+
+Api.post('/getAssignments', (req, res) => {
+  const member = getValueFromReferer(req.headers.referer, 'nobushi');
+
+  res.status(200).json(db.get('members').get(member).get('getAssignments').value());
+});
+
+Api.post('/assignAssessment', (req, res) => {
+  const member = getValueFromReferer(req.headers.referer, 'nobushi');
+
+  res.status(200).json(db.get('members').get(member).get('assignAssessment').value());
 });
 
 export default Api;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const Index = express.Router();
 
 Index.all('*', cors());
-Index.get(/^(?!\/(api|mock)).*$/, (req, res) => {
+Index.get(/^(?!\/(api)).*$/, (req, res) => {
   res.render('index');
 });
 
